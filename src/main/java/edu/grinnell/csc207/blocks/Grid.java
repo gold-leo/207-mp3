a8759e12b7e06540a6df0671663332b8556f6d2d
leo-goldman <goldmanleo@grinnell.edu>
2024-09-20 01:51:59 -0500

    added row, height, and width methods

--- src/main/java/edu/grinnell/csc207/blocks/Grid.java
+++ src/main/java/edu/grinnell/csc207/blocks/Grid.java
@@ -62,7 +62,7 @@ public class Grid implements AsciiBlock {
    *   If the row is invalid.
    */
   public String row(int i) throws Exception {
-    throw new Exception("Not yet implemented"); // STUB
+    return this.element.row(i % element.height()).repeat(hreps);
   } // row(int)
 
   /**
@@ -71,7 +71,7 @@ public class Grid implements AsciiBlock {
    * @return the number of rows
    */
   public int height() {
-    return 0;   // STUB
+    return vreps * element.height();
   } // height()
 
   /**
@@ -80,7 +80,7 @@ public class Grid implements AsciiBlock {
    * @return the number of columns
    */
   public int width() {
-    return 0;   // STUB
+    return hreps * element.width();
   } // width()
 
   /**
