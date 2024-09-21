2aa9a20b0e96630bfb22680d1f44fd5b41a28b90
KevinTang <tangkevi@grinnell.edu>
2024-09-20 20:14:26 -0500

    add Horizontal Composition

--- src/main/java/edu/grinnell/csc207/blocks/HComp.java
+++ src/main/java/edu/grinnell/csc207/blocks/HComp.java
@@ -71,8 +71,38 @@ public class HComp implements AsciiBlock {
    *   if i is outside the range of valid rows.
    */
   public String row(int i) throws Exception {
-    return "";  // STUB
+    if (i < 0 || i > this.height()) {
+      throw new Exception("Illegal Row Number");
+    } //If Statement
+    
+    StringBuilder rowAdj = new StringBuilder();
+    for (int j = 0; j < this.blocks.length; j++) {
+      int currHeight = this.blocks[j].height();
+      int currWidth = this.blocks[j].width();
+      int numAdj = HAdj(currHeight);
+      if (i < numAdj || i >= numAdj + currHeight) {
+        rowAdj.append(" ".repeat(currWidth));
+      } else {
+        rowAdj.append(this.blocks[j].row(i - numAdj));
+      } //If Statement
+    } //For Loop
+    return rowAdj.toString();
   } // row(int)
+
+
+  private int HAdj (int height) {
+    int standard = this.height();
+    switch (align) {
+      case TOP:
+        return 0;
+      case BOTTOM:
+        return standard - height;
+      case CENTER:
+        return (standard - height) / 2;
+      default:
+        return 0;
+    }
+  }
 
   /**
    * Determine how many rows are in the block.
@@ -80,7 +110,11 @@ public class HComp implements AsciiBlock {
    * @return the number of rows
    */
   public int height() {
-    return 0;   // STUB
+    int height = 0;
+    for (int i = 0; i < this.blocks.length; i++) {
+      height = Math.max(height, this.blocks[i].height());
+    }
+    return height;
   } // height()
 
   /**
@@ -89,8 +123,15 @@ public class HComp implements AsciiBlock {
    * @return the number of columns
    */
   public int width() {
-    return 0;   // STUB
+    int width = 0;
+    for (int i = 0; i < this.blocks.length; i++) {
+      width += this.blocks[i].width();
+    }
+    return width;
   } // width()
+
+
+
 
   /**
    * Determine if another block is structurally equivalent to this block.
@@ -102,6 +143,28 @@ public class HComp implements AsciiBlock {
    *    false otherwise.
    */
   public boolean eqv(AsciiBlock other) {
-    return false;       // STUB
+    return ((other instanceof HComp) && (this.eqv((HComp) other)));
   } // eqv(AsciiBlock)
+
+
+  /**
+   * Determine if another grid is structurally equivalent to this grid.
+   *
+   * @param other
+   *   The grid to compare to this grid.
+   *
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
+   */
+  public boolean eqv(HComp other) {
+    if (!(this.align == other.align && this.blocks.length == other.blocks.length)) {
+      return false;
+    }
+    for (int i = 0; i < this.blocks.length; i++) {
+      if (!this.blocks[i].eqv(other.blocks[i])) {
+        return false;
+      }
+    }
+    return true;
+  } // eqv(Grid)
 } // class HComp
