7f53fc964e99a64f2c5849005848e5ce0f85c7e9
gold-leo <goldmanleo@grinnell.edu>
2024-09-24 15:13:31 -0500

    eqv method for Rect

--- src/main/java/edu/grinnell/csc207/blocks/Rect.java
+++ src/main/java/edu/grinnell/csc207/blocks/Rect.java
@@ -98,7 +98,7 @@ public class Rect implements AsciiBlock {
    *    false otherwise.
    */
   public boolean eqv(AsciiBlock other) {
-    return false;       // STUB
+    return (other instanceof Rect) && (AsciiBlock.equal(this, other));
   } // eqv(AsciiBlock)
 
   // +---------------+-----------------------------------------------
