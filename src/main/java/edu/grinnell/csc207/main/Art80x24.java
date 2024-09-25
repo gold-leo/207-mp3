118fe6dd513b1835759f13e6fe43deb4673309ab
gold-leo <goldmanleo@grinnell.edu>
2024-09-24 23:28:42 -0500

    added art, removed sandbox

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -1,7 +1,14 @@
 package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.blocks.AsciiBlock;
+import edu.grinnell.csc207.blocks.Boxed;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.HComp;
+import edu.grinnell.csc207.blocks.Line;
+import edu.grinnell.csc207.blocks.Padded;
 import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.VComp;
 
 import java.io.PrintWriter;
 
@@ -23,7 +30,38 @@ public class Art80x24 {
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
-    AsciiBlock art = new Rect('^', 80, 24);
+
+    AsciiBlock m = new Rect('#', 12, 2);
+    AsciiBlock s = new Rect(' ', 10, 1);
+    AsciiBlock light = new VComp(HAlignment.CENTER, new AsciiBlock[] {m, s, m});
+
+    AsciiBlock g = new Rect('%', 30, 5);
+    AsciiBlock grill = new Boxed(g);
+    AsciiBlock sp = new Rect(' ', 3, 1);
+
+    AsciiBlock f = new HComp(VAlignment.TOP, new AsciiBlock[] {light, sp, grill, sp, light});
+    AsciiBlock front = new Boxed(f);
+
+    AsciiBlock b = new Rect('&', 68, 1);
+    AsciiBlock b2 = new Rect('*', 68, 1);
+  
+
+    AsciiBlock wheel = new Rect('&', 4, 4);
+    AsciiBlock spa = new Rect(' ', 52, 6);
+    AsciiBlock stands = new HComp(VAlignment.CENTER, new AsciiBlock[] {wheel, spa, wheel});
+
+    AsciiBlock five = new Line("/                                                \\");
+    AsciiBlock four = new Line("/                                              \\");
+    AsciiBlock three = new Line("/                                            \\");
+    AsciiBlock two = new Line("/                                          \\");
+    AsciiBlock one = new Line("/****************************************\\");
+
+    AsciiBlock window = new VComp(HAlignment.CENTER, new AsciiBlock[] {one, two, three, four, five});
+
+
+
+    AsciiBlock car = new VComp(HAlignment.CENTER, new AsciiBlock[] {window, front, b2, b, stands});
+    AsciiBlock art = new Padded(car, ' ', HAlignment.CENTER, VAlignment.CENTER, 80, 24);
     AsciiBlock.print(pen, art);
     pen.close();
   } // main(String[])
